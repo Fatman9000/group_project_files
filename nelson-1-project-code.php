ae201f5508a577a578513860c3f017f33af812a7
fatman9000 <conman0503@gmail.com>
2021-12-12 16:05:11 -0500

    updated

--- nelson-1-project-code.php
+++ nelson-1-project-code.php
@@ -23,12 +23,26 @@
   $phone = $_POST["phone"];
   $adult = $_POST["adultTickets"];
   $child = $_POST["childTickets"];
+  $date = $_POST["date"];
   $subtotal = ($adult * 35) + ($child * 30);
-  $totalCost = $subtotal + ($subtotal * TAX);
+  $salesTax = $subtotal * TAX;
+  if (($adult + $child) < 6 ) {
+    $fee = ($adult + $child)*1;
+  }
+  else {
+    $fee = ($adult + $child)*0.5;
+  }
+  $totalCost = $subtotal + $salesTax + $fee;
   echo ("<p>Thank you <b> ${name} </b> at <b> ${phone} </b>.  Details of your total cost <b>$" . number_format($totalCost, 2) . "</b> are shown below:</p>");
   echo ("<ul>");
-  echo ("<li>Sub-total: $" . number_format($subtotal, 2) . "</li>");
-  echo ("</ul>");
+  echo("<li>Adult Tickets : ${adult}</li>");
+  echo("<li>Child Tickets : ${child}</li>");
+  echo("<li>Date : ${date}</li>");
+  echo("<li>Sub-total: $" . number_format($subtotal, 2) . "</li>");
+  echo("<li>Sales Tax : $" . number_format($salesTax,2). "</li>");
+  echo("<li>Fee : $" . number_format($fee,2). "</li>");
+  echo("</ul>");
+  echo("<h3>Total: $". number_format($totalCost, 2). "<h3>");
   print("<br>");
   echo("<a href='nelson-1-project-form.html'>return to form<a>");
   ?>
